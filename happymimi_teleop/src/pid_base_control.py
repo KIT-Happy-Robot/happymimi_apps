37f47c0f409b31b529d2bff79f0cf8eb2dcd7ea0
HappyYusuke <c1005073@planet.kanazawa-it.ac.jp>
2023-02-05 18:35:30 +0900

    rotateAngleにP制御を追加

--- happymimi_teleop/src/pid_base_control.py
+++ happymimi_teleop/src/pid_base_control.py
@@ -12,6 +12,7 @@ import rospy
 import time
 import math
 import tf
+import numpy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
@@ -33,6 +34,7 @@ class BaseControl():
         self.target_deg = 0.0   # 目標の角度（度数法）
         self.remain_deg = 0.0
         self.sub_target_deg = 0.0
+        self.judg_deg = 999.0
 
     def debag(self):
         rospy.Subscriber('/teleop/translate', Float64, self.translateDist)
@@ -55,39 +57,56 @@ class BaseControl():
             pass
         #print(self.current_deg)
 
-    def publishTwist(self):
-        if self.twist_value.angular.z > 0.0:
-            print("rotateAngle")
-            while not rospy.is_shutdown():
-                rospy.sleep(0.1)
-                if self.sub_target_deg == 0.0 and self.current_deg >= self.target_deg:
-                    break
-                elif self.sub_target_deg != 0.0 and (self.sub_target_deg - 1.0) < self.current_deg and (self.sub_target_deg + 2.0) > self.current_deg:
-                    break
-                else:
-                    self.twist_pub.publish(self.twist_value)
-        elif self.twist_value.angular.z < 0.0:
-            print("rotateAngle")
-            while not rospy.is_shutdown():
-                rospy.sleep(0.1)
-                if self.sub_target_deg == 0.0 and self.current_deg <= self.target_deg:
-                    break
-                elif self.sub_target_deg != 0.0 and (self.sub_target_deg - 1.0) < self.current_deg and (self.sub_target_deg + 2.0) > self.current_deg:
-                    break
-                else:
-                    self.twist_pub.publish(self.twist_value)
-        else:
-            print("translateDist")
-            start_time = time.time()
-            end_time = time.time() + 0.15 # 誤差をカバーする0.15
-            while end_time - start_time <= self.target_time:
-                #print(end_time - start_time)
-                self.twist_pub.publish(self.twist_value)
-                end_time = time.time()
-                rospy.sleep(0.1)
+    def publishLinerX(self):
+        print("translateDist")
+        start_time = time.time()
+        end_time = time.time() + 0.15 # 誤差をカバーする0.15
+        while end_time - start_time <= self.target_time:
+            #print(end_time - start_time)
+            self.twist_pub.publish(self.twist_value)
+            end_time = time.time()
+            rospy.sleep(0.1)
         self.twist_value.linear.x = 0.0
+        self.twist_pub.publish(self.twist_value)
+        print("Finish translateDist")
+
+    def publishAnglarZ(self):
+        vel_max = 0.3
+        ku = 0.06  # 限界感度
+        kp = ku/2
+        print("rotateAngle")
+        while int(self.judg_deg) != int(self.current_deg):
+            if self.sub_target_deg == 0.0:
+                self.judg_deg = self.target_deg
+                vel_z = kp*(self.target_deg - self.current_deg)
+            # 0度を時計回りにまたぐとき
+            elif self.sub_target_deg > 180:
+                self.judg_deg = self.sub_target_deg
+                if abs(360 - self.sub_target_deg) < 10.0:
+                    vel_max = 0.2
+                if self.current_deg < 180:
+                    vel_z = -vel_max
+                else:
+                    vel_z = kp*(self.sub_target_deg - self.current_deg)
+            # 0度を反時計回りにまたぐとき
+            else:
+                self.judg_deg = self.sub_target_deg
+                if abs(0 - self.sub_target_deg) < 10.0:
+                    vel_max = 0.2
+                if self.current_deg > 180:
+                    vel_z = vel_max
+                else:
+                    vel_z = kp*(self.sub_target_deg - self.current_deg)
+
+            if abs(vel_z) > vel_max:
+                vel_z = numpy.sign(vel_z)*vel_max
+            self.twist_value.angular.z = vel_z
+            self.twist_pub.publish(self.twist_value)
+            rospy.sleep(0.1)
+
         self.twist_value.angular.z = 0.0
         self.twist_pub.publish(self.twist_value)
+        print("Finish rotateAngle")
         #print("final deg: " + str(self.current_deg))
 
     def translateDist(self, dist, speed = 0.2):
@@ -98,8 +117,7 @@ class BaseControl():
         speed = abs(speed)
         self.target_time = abs(dist / speed)
         self.twist_value.linear.x = dist/abs(dist)*speed
-        self.twist_value.angular.z = 0.0
-        self.publishTwist()
+        self.publishLinerX()
 
     def rotateAngle(self, deg, speed = 0.2):
         try:
@@ -126,10 +144,11 @@ class BaseControl():
         print("current deg: " + str(self.current_deg))
         print("target deg: " + str(self.target_deg))
         print("sub_target deg: " + str(self.sub_target_deg))
-        self.publishTwist()
+        self.publishAnglarZ()
 
 if __name__ == '__main__':
-    rospy.init_node('base_control')
+    rospy.init_node('pid_base_control')
     base_control = BaseControl()
-    base_control.debag()
-    rospy.spin()
+    base_control.rotateAngle(10)
+    #base_control.debag()
+    #rospy.spin()
