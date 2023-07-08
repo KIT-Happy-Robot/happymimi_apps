223d97903472030c40b3f2ea62ab0c06ccebf30e
Hiroto <c1115332@planet.kanazawa-it.ac.jp>
2023-07-08 14:28:42 +0900

    add: apps2/* -> foxy_dev branch

--- chaser19/scripts/chaser19.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-xterm -geometry 80x5+0+0   -e "/opt/ros/melodic/bin/roslaunch happymimi_bringup minimal.launch" &
-sleep 5s
-xterm -geometry 80x5+0+130 -e "/opt/ros/melodic/bin/rosrun urg_node urg_node" &
-sleep 5s
-#xterm -geometry 80x5+0+360 -e "/opt/ros/kinectic/bin/rosbag record -a" &
-#sleep 5s
-xterm -geometry 80x5+0+910 -e "~/test_ws/devel/lib/chaser19/chaser19"
